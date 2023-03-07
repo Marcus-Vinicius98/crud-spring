1adbd4d6229eb1c3b809455d7d6e9d21be24af33
Marcus Vinicius <marcusviniferrer@gmail.com>
2023-03-06 21:45:42 -0300

    classe course criada

---
+++ src/main/java/com/api/crudspring/model/Course.java
@@ -0,0 +1,63 @@
+package com.api.crudspring.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cursos")
+public class Course implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "nome", length = 200, nullable = false)
+	private String name;
+
+	@Column(name = "categoria", length = 10, nullable = false)
+	private String category;
+
+	public Course() {
+
+	}
+
+	public Course(Long id, String name, String category) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.category = category;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+}
